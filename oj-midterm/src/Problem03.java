9d8e7873e01791d10d50e53cadcd3c4a6eec843a
Dzheentaev Bektur <dzheentaev_b@auca.kg>
2023-02-10 18:51:13 +0600

    OJ 03 Accepted

---
+++ oj-midterm/src/Problem03.java
@@ -0,0 +1,27 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Problem03 {
+    public static void main(String args[]){
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        String s1 = in.nextLine();
+        String abc = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        for(int i =0;i<n;i++){
+            String s = in.nextLine();
+
+            int num = Integer.parseInt(in.nextLine());
+            String res = "";
+            for(int j = 0 ; j<s.length();j++){
+                int pos = (int)s.charAt(j)-65;
+                if(pos-num<0){
+                    res+=abc.charAt(26+pos-num);
+                }
+                else
+                     res+=abc.charAt((pos-num));
+            }
+            System.out.println(res);
+        }
+    }
+}
