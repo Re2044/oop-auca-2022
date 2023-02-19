b764caf66f09d33276d30a61078ababa5152d574
Dzheentaev Bektur <dzheentaev_b@auca.kg>
2023-02-19 11:36:38 +0600

    URI 1030 Accepted

--- uri-midterm/src/URI1030.java
+++ uri-midterm/src/URI1030.java
@@ -4,35 +4,20 @@ import java.util.List;
 import java.util.Scanner;
 
 public class URI1030 {
+    public static int func(int n, int k) {
+        if (n == 1) {
+            return 1;
+        } else {
+            return (func(n-1, k) + k-1) % n + 1;
+        }
+    }
     public static void main(String args[]){
         Scanner in= new Scanner(System.in);
         int len = in.nextInt();
-        BitSet arr = new BitSet(10000);
         for(int i = 0 ; i < len; i++){
             int n = in.nextInt();
             int m = in.nextInt();
-            int currPos = 0 ;
-            int algPos = 1;
-            arr.set(0,n,true);
-            while(arr.cardinality()>1){
-                while(algPos<=m-1){
-                    currPos++;
-                    currPos%=n;
-                    if(arr.get(currPos)==true){
-                        algPos++;
-                    }
-                }
-                algPos = 1;
-                arr.set(currPos,false);
-                currPos++;
-                currPos%=n;
-                if(arr.nextSetBit(currPos)!=-1){
-                    currPos=arr.nextSetBit(currPos);
-                }
-                else
-                    currPos = arr.nextSetBit(0);
-            }
-            System.out.println("Case "+(i+1)+": "+(currPos+1));
+            System.out.println("Case "+(i+1)+": "+(func(n,m)));
         }
     }
 }
