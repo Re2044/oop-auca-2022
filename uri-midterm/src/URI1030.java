b35216387f3e6d6f65e9011a202d6b685d032b78
Dzheentaev Bektur <dzheentaev_b@auca.kg>
2023-02-19 11:06:42 +0600

    URI 1030 Wrong Answer

--- uri-midterm/src/URI1030.java
+++ uri-midterm/src/URI1030.java
@@ -1,33 +1,33 @@
+import java.util.ArrayList;
 import java.util.BitSet;
+import java.util.List;
 import java.util.Scanner;
 
 public class URI1030 {
+    static void func(List<Integer>alive, int step, int currPos)
+    {
+        if (alive.size() == 1) {
+            System.out.println(alive.get(0));
+        }
+        else{
+            currPos = ((currPos + step -1) % alive.size());
+            alive.remove(currPos);
+            func(alive, step , currPos);
+        }
+
+
+    }
     public static void main(String args[]){
         Scanner in= new Scanner(System.in);
         int len = in.nextInt();
         for(int i = 0 ; i < len; i++){
             int n = in.nextInt();
             int m = in.nextInt();
-            BitSet used = new BitSet(n);
-            used.set(0,n,true);
-
-            while(used.cardinality()!=1){
-                for(int j = 0 ; j<n;j++){
-                    if(j%m==0 && used.get(j)==true){
-                        if(j+1<n && used.nextSetBit(j+1)!=-1)
-                            used.set( used.nextSetBit(j+1),false);
-                        else if(used.cardinality()!=1){
-                            used.set(used.nextSetBit(0),false);
-                        }
-                        else
-                            break;
-                    }
-                }
-
-                // System.out.println(used.cardinality());
+            List<Integer> alive = new ArrayList<>(n);
+            for(int j = 1;j<=n;j++){
+                alive.add(j);
             }
-            System.out.println(used.nextSetBit(0)+1);
-          //  System.out.println(beg);
+            func(alive,m,0);
         }
     }
 }
