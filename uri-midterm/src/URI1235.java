27b10428534b2502f0ea9e02f52750d0e7ea8634
Dzheentaev Bektur <dzheentaev_b@auca.kg>
2023-02-26 17:25:28 +0600

    URI 1235 Presentation Error(100%)

---
+++ uri-midterm/src/URI1235.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class URI1235 {
+    public static void main(String args[]){
+        Scanner in = new Scanner(System.in);
+        int len = in.nextInt() ;
+        for(int i =0;i<len+1;i++) {
+            String curr = in.nextLine();
+            StringBuilder first = new StringBuilder();
+            first.append(curr.substring(0,curr.length()/2));
+            first.reverse();
+            StringBuilder second = new StringBuilder();
+            second.append(curr.substring(curr.length()/2,curr.length()));
+            second.reverse();
+            System.out.println(first.append(second));
+        }
+    }
+}
