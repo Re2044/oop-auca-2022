7761ccf5a1453134d5bbc0e4aaa6cccfedf6e90d
Dzheentaev Bektur <dzheentaev_b@auca.kg>
2023-02-26 17:12:48 +0600

    URI 1234 Accepted

--- uri-midterm/src/URI1234.java
+++ uri-midterm/src/URI1234.java
@@ -3,22 +3,17 @@ import java.util.Scanner;
 public class URI1234 {
     public static void main(String args[]){
         Scanner in = new Scanner(System.in);
-        while(in.hasNext()){
+        while(in.hasNextLine()){
             String curr = in.nextLine();
-            if(curr.isEmpty()){
-                break;
-            }
-            int zero = 0;
             int cnt = 0;
             String res = "";
             for(int i = 0;i<curr.length();i++){
                 if(curr.charAt(i)!=' '){
-                    zero++;
                     if(cnt%2==0){
                         res+=Character.toUpperCase(curr.charAt(i));
                     }
                     else
-                        res+=curr.charAt(i);
+                        res+=Character.toLowerCase(curr.charAt(i));
                     cnt++;
                 }
                 else{
