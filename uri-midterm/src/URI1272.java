59d4f735a03db4ba1454c1a6feb81212c24db542
Dzheentaev Bektur <dzheentaev_b@auca.kg>
2023-02-26 17:47:55 +0600

    URI 1272 WA(100%)

---
+++ uri-midterm/src/URI1272.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class URI1272 {
+    public static void main(String args[]){
+        Scanner in = new Scanner (System.in);
+        int len = in.nextInt();
+        String s = in.nextLine();
+        for(int i = 0 ; i <len ; i++){
+            String input = in.nextLine();
+            String[]ins = input.split("·");
+            String res = "";
+            for(int j  = 0;j<ins.length;j++){
+                if(ins[j]!="·" && ins[j].isEmpty()==false){
+                    res+=ins[j].charAt(0);
+                }
+            }
+            System.out.println(res);
+        }
+    }
+}
