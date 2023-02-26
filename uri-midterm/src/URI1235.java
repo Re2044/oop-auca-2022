2fdde2c9ee3d8ddf1e2987c101eb7fa4ff76cd8d
Dzheentaev Bektur <dzheentaev_b@auca.kg>
2023-02-26 17:26:40 +0600

    URI 1235 Accepted

--- uri-midterm/src/URI1235.java
+++ uri-midterm/src/URI1235.java
@@ -4,7 +4,8 @@ public class URI1235 {
     public static void main(String args[]){
         Scanner in = new Scanner(System.in);
         int len = in.nextInt() ;
-        for(int i =0;i<len+1;i++) {
+        String s = in.nextLine();
+        for(int i =0;i<len;i++) {
             String curr = in.nextLine();
             StringBuilder first = new StringBuilder();
             first.append(curr.substring(0,curr.length()/2));
