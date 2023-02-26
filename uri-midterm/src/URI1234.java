e379b00994338d91eb231c39b40d2056d9b376ab
Dzheentaev Bektur <dzheentaev_b@auca.kg>
2023-02-26 16:58:06 +0600

    URI 1234 Presentation error (100%)

--- uri-midterm/src/URI1234.java
+++ uri-midterm/src/URI1234.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class URI1234 {
     public static void main(String args[]){
         Scanner in = new Scanner(System.in);
-        while(in.hasNext()){
+        while(in.hasNextLine()){
             String curr = in.nextLine();
             int cnt = 0;
             String res = "";
