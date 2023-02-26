24b909cb39bdcbdbfcf9bd5744d1f6729e6ed99d
Dzheentaev Bektur <dzheentaev_b@auca.kg>
2023-02-26 16:56:40 +0600

    URI 1234 Presentation error (100%)

---
+++ uri-midterm/src/URI1234.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class URI1234 {
+    public static void main(String args[]){
+        Scanner in = new Scanner(System.in);
+        while(in.hasNext()){
+            String curr = in.nextLine();
+            int cnt = 0;
+            String res = "";
+            for(int i = 0;i<curr.length();i++){
+                if(curr.charAt(i)!=' '){
+                    if(cnt%2==0){
+                        res+=Character.toUpperCase(curr.charAt(i));
+                    }
+                    else
+                        res+=curr.charAt(i);
+                    cnt++;
+                }
+                else{
+                    res+=curr.charAt(i);
+                }
+            }
+            System.out.println(res);
+        }
+    }
+}
