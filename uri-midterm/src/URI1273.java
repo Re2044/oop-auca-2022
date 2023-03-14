a2683a31a2f9cfbb1cfe0d098b8193783bdf248e
Dzheentaev Bektur <dzheentaev_b@auca.kg>
2023-03-14 19:08:34 +0600

    URI 1273 Accepted(-_-)

--- uri-midterm/src/URI1273.java
+++ uri-midterm/src/URI1273.java
@@ -3,11 +3,14 @@ import java.util.Scanner;
 public class URI1273 {
     public static void main(String args[]){
         Scanner in = new Scanner(System.in);
+        boolean flag= false;
         while(true){
             int len = in.nextInt();
             if(len==0){
                 break;
             }
+            if(flag==true)
+                System.out.println();
             String s = in.nextLine();
             String[] ans = new String[len];
             int maxx = -1;
@@ -21,7 +24,7 @@ public class URI1273 {
                     System.out.print(' ');
                 System.out.println(ans[i]);
             }
-            System.out.println();
+            flag =true;
         }
     }
 }
