77eb9e1f0e3fed79b2798dab1fe4900b12e8a8ca
Dzheentaev Bektur <dzheentaev_b@auca.kg>
2023-03-14 17:52:09 +0600

    URI2062 Presentation error (100%)

---
+++ uri-midterm/src/URI2062.java
@@ -0,0 +1,27 @@
+
+import java.util.Scanner;
+
+public class URI2062 {
+    public static void main(String args[]){
+        Scanner in = new Scanner(System.in);
+        int len = in.nextInt();
+        String s = in.nextLine();
+        String input = in.nextLine();
+        String[]ins = input.split(" ");
+        for(int i = 0;i<ins.length;i++){
+            if(ins[i].length()==3){
+
+                if(ins[i].substring(0,2).equals("UR")){
+
+                    ins[i] = "URI";
+                }
+                if(ins[i].substring(0,2).equals("OB")){
+                    ins[i] ="OBI";
+                }
+            }
+        }
+        for(int i = 0;i<ins.length;i++) {
+            System.out.print(ins[i]+" ");
+        }
+    }
+}
