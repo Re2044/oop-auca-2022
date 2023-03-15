1df497dffa53f150399fd16b25ad4c057c4236d0
Dzheentaev Bektur <dzheentaev_b@auca.kg>
2023-03-15 12:14:38 +0600

    URI 1237 Wrong Answer(100%)

--- uri-midterm/src/URI1237.java
+++ uri-midterm/src/URI1237.java
@@ -8,16 +8,12 @@ public class URI1237 {
             String second = in.nextLine();
             int res = 0;
             int len = Math.min(first.length(),second.length());
-            for(int i = len-1;i>=0;i--){
+            for(int i = len-1;i>0;i--){
                 for(int j =0 ;j < first.length()-i+1;j++){
                     String subst = first.substring(j,j+i);
-                    for(int k = 0;k<second.length()-i+1;k++){
-                        String sub = second.substring(k,k+i);
-                       // System.out.println(subst + " " +sub);
-                        if(subst.equals(sub)){
-                            res = Math.max(res,i);
-                            break;
-                        }
+                    if(second.contains(subst)){
+                        res = Math.max(res,i);
+                        break;
                     }
                 }
             }
