55890ccd9878f140249b2a937f092762c480af6d
Dzheentaev Bektur <dzheentaev_b@auca.kg>
2023-04-04 21:47:00 +0600

    URI 1768 Presentation error(100%)

--- uri-midterm/src/URI1768.java
+++ uri-midterm/src/URI1768.java
@@ -7,36 +7,43 @@ public class URI1768 {
         int k = 0;
         while(in.hasNextLine()){
             int x = Integer.parseInt(in.nextLine());
-
-            for(int i = 1;i<=x;i+=2){
-                for(int j = 0;j<(x-i)/2;j++){
+            if(x==1){
+                System.out.println(" * ");
+                System.out.println("***");
+                System.out.println();
+            }
+            else {
+                for(int i = 1;i<=x;i+=2){
+                    for(int j = 0;j<(x-i)/2;j++){
+                        System.out.print(" ");
+                    }
+                    for(int j = 0;j<i;j++){
+                        System.out.print("*");
+                    }
+                    for(int j = 0;j<(x-i)/2;j++){
+                        System.out.print(" ");
+                    }
+                    System.out.println();
+                }
+                for(int j = 0;j<(x-1)/2;j++){
                     System.out.print(" ");
                 }
-                for(int j = 0;j<i;j++){
-                    System.out.print("*");
-                }
-                for(int j = 0;j<(x-i)/2;j++){
+                System.out.print("*");
+                for(int j = 0;j<(x-1)/2;j++){
                     System.out.print(" ");
                 }
                 System.out.println();
+                for(int j = 0;j<(x-3)/2;j++){
+                    System.out.print(" ");
+                }
+                System.out.print("***");
+                for(int j = 0;j<(x-3)/2;j++){
+                    System.out.print(" ");
+                }
+                System.out.println();
+                System.out.println();
             }
-            for(int j = 0;j<(x-1)/2;j++){
-                System.out.print(" ");
-            }
-            System.out.print("*");
-            for(int j = 0;j<(x-1)/2;j++){
-                System.out.print(" ");
-            }
-            System.out.println();
-            for(int j = 0;j<(x-3)/2;j++){
-                System.out.print(" ");
-            }
-            System.out.print("***");
-            for(int j = 0;j<(x-3)/2;j++){
-                System.out.print(" ");
-            }
-            System.out.println();
-            System.out.println();
+
         }
     }
 }
