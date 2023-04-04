306d51d410f1d93efb85d1c928c388f46d2e2025
Dzheentaev Bektur <dzheentaev_b@auca.kg>
2023-04-04 21:33:06 +0600

    URI 1768 Runtime error

---
+++ uri-midterm/src/URI1768.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class URI1768 {
+    public static  void main(String args[]){
+        Scanner in = new Scanner(System.in);
+        while(in.hasNextLine()){
+            int x = in.nextInt();
+            for(int i = 1;i<=x;i+=2){
+                for(int j = 0;j<(x-i)/2;j++){
+                    System.out.print(" ");
+                }
+                for(int j = 0;j<i;j++){
+                    System.out.print("*");
+                }
+                for(int j = 0;j<(x-i)/2;j++){
+                    System.out.print(" ");
+                }
+                System.out.println();
+            }
+            for(int j = 0;j<(x-1)/2;j++){
+                System.out.print(" ");
+            }
+            System.out.print("*");
+            for(int j = 0;j<(x-1)/2;j++){
+                System.out.print(" ");
+            }
+            System.out.println();
+            for(int j = 0;j<(x-3)/2;j++){
+                System.out.print(" ");
+            }
+            System.out.print("***");
+            for(int j = 0;j<(x-3)/2;j++){
+                System.out.print(" ");
+            }
+            System.out.println();
+            System.out.println();
+        }
+    }
+}
