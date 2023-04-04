b3409ae341ac81c0eaebb1ab66d59281e87ae5d2
Dzheentaev Bektur <dzheentaev_b@auca.kg>
2023-04-04 20:56:47 +0600

    URI 1217 Wrong Answer(100%)

---
+++ uri-midterm/src/URI1217.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+import java.util.Locale;
+public class URI1217 {
+    public static void main(String args[]){
+        Scanner in = new Scanner(System.in).useLocale(Locale.US);;
+        int n = in.nextInt();
+        double sum = 0;
+        int much  =0;
+
+        for(int i = 1;i<=n;i++){
+            double amount =in.nextDouble();
+            String y = in.nextLine();
+            String fruits = in.nextLine();
+            int len = (fruits.split(" ")).length;
+            much+=len;
+            sum+=amount;
+            System.out.println("day 1: "+len+" kg");
+        }
+        System.out.println(Math.round(((double) much/n)*100.0)/100.0+" kg by day");
+        System.out.println("R$ "+Math.round(((double) sum/n)*100.0)/100.0+" by day");
+    }
+}
