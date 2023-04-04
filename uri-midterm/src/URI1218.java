bcbd80d28a8506aa7be5fb53bff9494d15277a95
Dzheentaev Bektur <dzheentaev_b@auca.kg>
2023-04-04 21:17:29 +0600

    URI 1218 Accepted

--- uri-midterm/src/URI1218.java
+++ uri-midterm/src/URI1218.java
@@ -5,12 +5,12 @@ public class URI1218 {
         Scanner in = new Scanner(System.in);
         int i = 1;
         while(in.hasNextLine()){
+            if(i!=1)
+                System.out.println();
             int search = Integer.parseInt(in.nextLine());
             String it = in.nextLine();
 
             String ins[] = it.split(" ");
-
-            System.out.println("Caso "+i+":");
             int cntf = 0;
             int cntm = 0;
             for(int j = 0;j<ins.length;j+=2){
@@ -22,10 +22,11 @@ public class URI1218 {
                         cntm++;
                 }
             }
+            System.out.println("Caso "+i+":");
             System.out.println("Pares Iguais: "+(cntf+cntm));
             System.out.println("F: "+cntf);
             System.out.println("M: "+cntm);
-            //System.out.println();
+
             i++;
         }
     }
