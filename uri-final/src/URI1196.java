69e304eb7cad8c3fbb577ed3dfc37403238eec47
Dzheentaev Bektur <dzheentaev_b@auca.kg>
2023-04-12 13:48:09 +0600

    URI 1196 Time limit exceeded

--- uri-final/src/URI1196.java
+++ uri-final/src/URI1196.java
@@ -6,17 +6,21 @@ import java.util.Scanner;
 public class URI1196 {
     public static void main(String args[]){
         Scanner in = new Scanner(System.in);
-        String s = in.nextLine();
-        String qwe = "`1234567890-=QWERTYUIOP[]\\\\ASDFGHJKL;'ZXCVBNM,./";
-        for (int i = 0 ;i<s.length();i++){
-            int index = qwe.indexOf(s.charAt(i));
-            if(index!=-1){
-                System.out.print(qwe.charAt(index-1));
+        while(in.hasNextLine()){
+            String s = in.nextLine();
+            String qwe = "`1234567890-=QWERTYUIOP[]\\\\ASDFGHJKL;'ZXCVBNM,./";
+            for (int i = 0 ;i<s.length();i++){
+                int index = qwe.indexOf(s.charAt(i));
+                if(index!=-1){
+                    System.out.print(qwe.charAt(index-1));
+                }
+                else{
+                    System.out.print(s.charAt(i));
+                }
             }
-            else{
-                System.out.print(s.charAt(i));
-            }
+            System.out.println();
         }
+
 
     }
 }
