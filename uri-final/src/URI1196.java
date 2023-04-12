caba5568edb324091a9a0a2c5af60dd34ea603d0
Dzheentaev Bektur <dzheentaev_b@auca.kg>
2023-04-12 13:38:42 +0600

    URI 1196 Wrong answer(100%)

--- uri-final/src/URI1196.java
+++ uri-final/src/URI1196.java
@@ -8,7 +8,6 @@ public class URI1196 {
         Scanner in = new Scanner(System.in);
         String s = in.nextLine();
         String qwe = "`1234567890-=QWERTYUIOP[]\\ASDFGHJKL;'ZXCVBNM,./";
-
         Map<Character,Integer> dict= new HashMap<Character,Integer>();
 
         for(int i = 0 ;i<qwe.length();i++){
@@ -17,8 +16,6 @@ public class URI1196 {
         String res = "";
         for (int i = 0 ;i<s.length();i++){
             if(s.charAt(i)!=' '){
-
-                //System.out.println(qwe.charAt(dict.get(s.charAt(i))-1));
                 res+=qwe.charAt(dict.get(s.charAt(i))-1);
             }
             else
