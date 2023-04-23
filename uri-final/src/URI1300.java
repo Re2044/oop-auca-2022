91c649e70a4aa0d22135b580a18d0951764e92a1
Dzheentaev Bektur <dzheentaev_b@auca.kg>
2023-04-23 13:03:43 +0600

    URI 1300 Accepted

--- uri-final/src/URI1300.java
+++ uri-final/src/URI1300.java
@@ -1,15 +1,22 @@
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public class URI1300 {
     public static void main(String args[]){
         Scanner in = new Scanner(System.in);
         while(in.hasNextLine()){
-            int n = in.nextInt();
-            if(n%6==0){
-                System.out.println("Y");
+            try{
+                int n = in.nextInt();
+                if(n%6==0){
+                    System.out.println("Y");
+                }
+                else
+                    System.out.println("N");
             }
-            else
-                System.out.println("N");
+            catch (NoSuchElementException ex){
+                break;
+            }
+
         }
     }
 }
