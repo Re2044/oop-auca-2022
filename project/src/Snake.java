11674901e5a75b1bf47ea5f8b381f70a3038312d
Dzheentaev Bektur <dzheentaev_b@auca.kg>
2023-05-26 12:25:49 +0600

    Defined move method for Snake

--- project/src/Snake.java
+++ project/src/Snake.java
@@ -2,7 +2,7 @@ import java.awt.*;
 import java.util.ArrayList;
 
 public class Snake {
-    private static int length = 10;
+    private int length;
     private ArrayList<Integer> arrayRow = new ArrayList<>();
     private ArrayList<Integer> arrayCol = new ArrayList<>();
     private int rowCount;
@@ -10,12 +10,12 @@ public class Snake {
     private String direction;
     private static Color color = Color.RED;
     Snake(int rowCount,int columnCount,String direction){
-
         this.direction = direction;
         this.rowCount = rowCount;
         this.columnCount = columnCount;
         arrayRow.add(0);
         arrayCol.add(0);
+        length = 1;
     }
     public void Paint(Graphics g, int width, int height){
         g.setColor(color);
@@ -24,5 +24,13 @@ public class Snake {
             int row = arrayRow.get(i);
             g.fillOval((width/columnCount)*col,(height/rowCount)*row,width/columnCount,height/rowCount);
         }
+    }
+    public void move(int nextRow,int nextCol){
+        if(length==10){
+            arrayCol.remove(0);
+            arrayRow.remove(0);
+        }
+        arrayCol.add(nextCol);
+        arrayRow.add(nextRow);
     }
 }
